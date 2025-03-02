08b3407cc02d48ebbd271848f9a2472ac742d766
Fallujahrama <fallujahrc@gmail.com>
2025-03-02 13:20:09 +0700

    push week3 progress pengerjaan js

---
+++ week3/PWL_POS/resources/views/user/show.blade.php
@@ -0,0 +1,5 @@
+<html>
+    <body>
+        <h1>Profil Pengguna {{ $name }} (ID: {{ $id }})</h1>
+    </body>
+</html>
---
+++ week3/PWL_POS/routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PenjualanController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [HomeController::class, 'home']);
+
+Route::get('/products', [ProductController::class, 'index']);
+
+Route::prefix('category')->group(function () {
+    Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
+    Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
+    Route::get('/home-care', [ProductController::class, 'homeCare']);
+    Route::get('/baby-kid', [ProductController::class, 'babyKid']);
+});
+
+// Route::get('/category/{category}', [ProductController::class, 'category']);
+
+Route::get('/user/{id}/name/{name}', [UserController::class, 'show']);
+
+Route::get('/penjualan', [PenjualanController::class, 'index']);
