1674af959b33bad53ce629f83a0fa9f67ea368ae
Fallujahrama <fallujahrc@gmail.com>
2025-03-08 16:36:57 +0700

    rsm where id 1

--- week4/PWL_POS/app/Http/Controllers/UserController.php
+++ week4/PWL_POS/app/Http/Controllers/UserController.php
@@ -22,7 +22,9 @@ class UserController extends Controller
 
         //coba akses model UserModel
         // $user = UserModel::all(); //mengambil semua data dari tabel m_user
-        $user = UserModel::find(1); //mengambil data dari tabel m_user dengan id 1
+        // $user = UserModel::find(1); //mengambil data dari tabel m_user dengan id 1
+        $user = UserModel::where('level_id', 1)->first(); //mengambil data dari tabel m_user dengan level_id 1
+        // $user = UserModel::firstWhere('level_id', 1); //mengambil data dari tabel m_user dengan level_id 1
         return view('user', ['data' => $user]); //mengirim data ke view user
     }
 }
