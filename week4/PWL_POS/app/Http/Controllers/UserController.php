a532f289380448cdafec091940c8c9df63742694
Fallujahrama <fallujahrc@gmail.com>
2025-03-08 16:42:56 +0700

    rsm findOr 1

--- week4/PWL_POS/app/Http/Controllers/UserController.php
+++ week4/PWL_POS/app/Http/Controllers/UserController.php
@@ -24,7 +24,11 @@ class UserController extends Controller
         // $user = UserModel::all(); //mengambil semua data dari tabel m_user
         // $user = UserModel::find(1); //mengambil data dari tabel m_user dengan id 1
         // $user = UserModel::where('level_id', 1)->first(); //mengambil data dari tabel m_user dengan level_id 1
-        $user = UserModel::firstWhere('level_id', 1); //mengambil data dari tabel m_user dengan level_id 1
+        // $user = UserModel::firstWhere('level_id', 1); //mengambil data dari tabel m_user dengan level_id 1
+
+        $user = UserModel::findOr(1, ['username', 'nama'], function (){ //mencari data dari tabel m_user dengan id 1 atau username dan nama
+            abort(404); //jika data tidak ditemukan, kembali dengan error 404
+        });
         return view('user', ['data' => $user]); //mengirim data ke view user
     }
 }
