eb645b2320719188bb684a257fc59f9824e4b410
Fallujahrama <fallujahrc@gmail.com>
2025-03-08 16:32:54 +0700

    rsm find 1

--- week4/PWL_POS/app/Http/Controllers/UserController.php
+++ week4/PWL_POS/app/Http/Controllers/UserController.php
@@ -9,19 +9,20 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index() {
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-            // 'nama' => 'Pelanggan Pertama', //eluqoent model
-        ];
-        UserModel::create($data); //menambahkan data ke tabel m_user
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345')
+        //     // 'nama' => 'Pelanggan Pertama', //eluqoent model
+        // ];
+        // UserModel::create($data); //menambahkan data ke tabel m_user
         // UserModel::insert($data); //menambahkan data ke tabel m_user
         // UserModel::where('username', 'customer-1')->update($data);
 
         //coba akses model UserModel
-        $user = UserModel::all(); //mengambil semua data dari tabel m_user
+        // $user = UserModel::all(); //mengambil semua data dari tabel m_user
+        $user = UserModel::find(1); //mengambil data dari tabel m_user dengan id 1
         return view('user', ['data' => $user]); //mengirim data ke view user
     }
 }
