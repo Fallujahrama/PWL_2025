1c15193d08ce92f3437d81a01cb9771f38d348e3
Fallujahrama <fallujahrc@gmail.com>
2025-03-24 22:01:58 +0700

    tugas js 5, tabel m_level

--- week5.2/PWL_POS/app/Models/LevelModel.php
+++ week5.2/PWL_POS/app/Models/LevelModel.php
@@ -4,17 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class LevelModel extends Model
 {
     use HasFactory;
 
-    protected $table = 'm_level'; 
+    protected $table = 'm_level';
     protected $primaryKey = 'level_id';
-    
-    public function user(): HasOne
-    {
-        return $this->hasOne(UserModel::class, 'level_id', 'level_id');
-    }
+    protected $guarded = [];
 }
--- week5.2/PWL_POS/routes/web.php
+++ week5.2/PWL_POS/routes/web.php
@@ -48,24 +48,26 @@ Route::group(['prefix' => 'user'], function () {
     Route::delete('/{id}', [UserController::class, 'destroy']);
 });
 
+// level controller
+Route::group(['prefix' => 'level'], function () {
+    Route::get('/', [LevelController::class, 'index']); // menampilkan halaman awal Level
+    Route::post('/list', [LevelController::class, 'list']); // menampilkan data Level dalam bentuk json untuk datatables
+    Route::get('/create', [LevelController::class, 'create']); // menampilkan halaman form tambah Level
+    Route::post('/', [LevelController::class, 'store']);  // menyimpan data Level baru
+    Route::get('/{id}', [LevelController::class, 'show']); // menampilkan detail Level
+    Route::get('/{id}/edit', [LevelController::class, 'edit']); // menampilkan halaman form edit Level
+    Route::put('/{id}', [LevelController::class, 'update']);  // menyimpan perubahan data Level
+    Route::delete('/{id}', [LevelController::class, 'destroy']); // menghapus data user
+});
+
 // Route::get('/category/{category}', [ProductController::class, 'category']);
-
 // Route::get('/user/{id}/name/{name}', [UserController::class, 'show']);
-
 Route::get('/penjualan', [PenjualanController::class, 'index']);
-
 Route::get('/level', [LevelController::class, 'index']);
-
 Route::get('/kategori', [KategoriController::class, 'index']);
-
 // Route::get('/user', [UserController::class, 'index']);
-
 // Route::get('/user/tambah', [UserController::class, 'tambah']);
-
 // Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
-
 // Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-
 // Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
-
 // Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
